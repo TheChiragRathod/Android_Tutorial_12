29c1ec5de33cb22731ff1403a758701f7d2e7f5a
TheChiragRathod <crathod116@rku.ac.in>
2020-10-07 20:48:52 +0530

    added volley library dipendency.

--- app/src/main/java/com/chirag/tutorial12/MainActivity.java
+++ app/src/main/java/com/chirag/tutorial12/MainActivity.java
@@ -1,15 +1,64 @@
 package com.chirag.tutorial12;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    JSONArray usersJSONArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("Users List");
+        setVollyAPI();
+
+    }
+
+
+
+
+    public void setVollyAPI()
+    {
+
+        //Volley Library Code Start Here........
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(Request.Method.GET, MyUtil.USER_URL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    usersJSONArray=response;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Somthing went Wrong...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
